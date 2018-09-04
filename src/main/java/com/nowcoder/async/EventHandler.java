5d1f978e0ff99e2ae14fcc58405e4556f8e48a33
tfl xin <1445971703@qq.com>
2018-09-04 11:58:50 +0800

    EventHandler接口：对于活动定义他的行为

--- src/main/java/com/nowcoder/async/EventHandler.java
+++ src/main/java/com/nowcoder/async/EventHandler.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 /**
  * Created by nowcoder on 2018/7/14.
+ EventHandler接口：对于活动定义他的行为
  抽象成接口处理，处理model，以及关注 getSupportEventTyp
  */
 public interface EventHandler {
-    void doHandle(EventModel model);
-    List<EventType> getSupportEventTypes();
+    void doHandle(EventModel model);//定义接口，针对活动要执行的动作
+    List<EventType> getSupportEventTypes();//关注的活动类型有哪些
 }
