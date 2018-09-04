deb402cee80504ae037627bdeecb7234cce2d74f
tfl xin <1445971703@qq.com>
2018-09-04 11:56:14 +0800

    EventProducer：活动生产者

--- src/main/java/com/nowcoder/async/EventProducer.java
+++ src/main/java/com/nowcoder/async/EventProducer.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * Created by nowcoder on 2018/7/14.
- 用来发送事件中的数据（EnventModel），把数据序列化后放到某个序列里面，然后放到Redis的某个 队列中
+ EventProducer：活动生产者，相当于生产消费者设计模式中的生产者，在controller层执行一个动作之后，
+                用这个类把需要异步的信息（用来发送事件中的数据（EnventModel））打包好，放入到redis队里中。
+ 利用json进行对象序列化和反序列化，然后json存放到Redis中，相当于存储在redis的某个 队列中
+ 同步的话容易受到io影响造成程序吞吐量降低，采用异步方式比较好，将活动打包，放入队列，
+ 后台有一个消费线程一直在拆包，执行
  */
 @Service
 public class EventProducer {
