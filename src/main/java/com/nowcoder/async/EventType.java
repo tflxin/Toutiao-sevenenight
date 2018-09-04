850e41c84b258484ae4b56c1955569ff7a40bf8d
tfl xin <1445971703@qq.com>
2018-09-04 11:59:10 +0800

    EventType，获得活动的类型

--- src/main/java/com/nowcoder/async/EventType.java
+++ src/main/java/com/nowcoder/async/EventType.java
@@ -2,7 +2,8 @@ package com.nowcoder.async;
 
 /**
  * Created by nowcoder on 2018/7/14.
- EventType表示刚刚发生了什么事件
+ EventType，获得活动的类型，可以有点赞，评论，登录等待
+ 表示刚刚发生了什么事件
  */
 public enum EventType {
     LIKE(0),
