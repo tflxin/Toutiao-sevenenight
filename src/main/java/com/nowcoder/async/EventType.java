9810c2105cebe1dcd88d86cf2a5d98b62402cb43
tfl xin <1445971703@qq.com>
2018-09-07 10:03:20 +0800

    活动的实体类型：包装类 包括异步的优点

--- src/main/java/com/nowcoder/async/EventType.java
+++ src/main/java/com/nowcoder/async/EventType.java
@@ -4,6 +4,10 @@ package com.nowcoder.async;
  * Created by nowcoder on 2018/7/14.
  EventType，获得活动的类型，可以有点赞，评论，登录等待
  表示刚刚发生了什么事件
+
+ 异步架构（优先队列，消费者线程，找到相应handler），若该部分异常，biz==eventProduceren仍然可以用
+
+ 优点：把复杂的业务处理的流程切割开，对及时反馈的数据进行反馈，对滞后的更新的数据全部通过事件的处理函数慢慢更新
  */
 public enum EventType {
     LIKE(0),
