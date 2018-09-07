41a2a51dfafd5f3f405c2a7bf32ff120a3279fe6
tfl xin <1445971703@qq.com>
2018-09-07 09:53:35 +0800

    like无模式，直接从JedisAdaptor中读取：封装好的包装类工具

--- src/main/java/com/nowcoder/controller/LikeController.java
+++ src/main/java/com/nowcoder/controller/LikeController.java
@@ -19,7 +19,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by nowcoder on 2018/7/13.
- 前台的入口
+ 点赞：给资讯关联一个like的集合，把userId放进去
+ 踩：加一disLike的集合，把userId放进去；
+ 采用redis的set类型的类型，不需要DAO层，不对数据库进行操作，
+ like无模式，直接从JedisAdaptor中读取：封装好的包装类工具
+ afterPropertiesSet()：set之前建一个新的线程池pool = new JedisPool("localhost", 6379);
+ //获取Jedis getJedis()， getJedis().get(key)；jedis.set(key, value);
+ //包装，把集合放进去jedis.sadd(key, value);
+ jedis.srem(key, value);jedis.sismember(key, value);
+ jedis.scard(key);//集合中人数 ，验证码设置setex(key, 10, value);
+ jedis.lpush(key, value);jedis.brpop(timeout, key);
+ //用一个json串保存
+ set(key, JSON.toJSONString(obj));
+
+ 前台的入口，把数据读取出来并加入一些入口
+ news里面的count需要更新
+ likeCount的形式反馈到前端
+ 更改数量：主页：HomeController： 更改getNews():vo.set(like,''')
+        资讯详情页newsController：newsDetail也需要做相应的更改：
+ int localUserId = hostHolder.getUser() != null ? hostHolder.getUser().getId() : 0;
  */
 @Controller
 public class LikeController {
@@ -42,7 +60,7 @@ public class LikeController {
         long likeCount = likeService.like(hostHolder.getUser().getId(), EntityType.ENTITY_NEWS, newsId);
         // 更新喜欢数
         News news = newsService.getById(newsId);
-        newsService.updateLikeCount(newsId, (int) likeCount);
+        newsService.updateLikeCount(newsId, (int)likeCount);
         //发送一个事件过来
         eventProducer.fireEvent(new EventModel(EventType.LIKE)
                 .setEntityOwnerId(news.getUserId())
